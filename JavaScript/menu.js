6c7d3faf67bfbf0e651a9d2013c525b84dfef51b
Etsalot <ettienne.swart95@gmail.com>
2024-04-26 10:43:25 +0200

    update

--- JavaScript/menu.js
+++ JavaScript/menu.js
@@ -1,18 +1,25 @@
-const root = "/WSOA3028A_1595043"
+const root = "/WSOA3028A_1595043/"
 const menuItems =
     [
-        { name: "Home", href: `${root}/index.html` },
-        { name: "Blog", href: `${root}/Blog Posts/Blogs.html` },
-        { name: "Essays", href: `${root}/Essay/Essay.html` },
-        { name: "Portfolio", href: `${root}/Portfolio/Portfolio.html` },
-        { name: "Profile", href: `${root}/Profile/about.html` },
-        { name: "Design", href: `${root}/Design/Design.html` },
-        { name: "Miscellaneous", href: `${root}/Miscellaneous/Misc.html` },
+        { name: "Home", href: `${root}index.html` },
+        { name: "Blog", href: `${root}Blog Posts/Blogs.html` },
+        { name: "Essays", href: `${root}Essay/Essay.html` },
+        { name: "Portfolio", href: `${root}Portfolio/Portfolio.html` },
+        { name: "Profile", href: `${root}Profile/about.html` },
+        { name: "Design", href: `${root}Design/Design.html` },
+        { name: "Miscellaneous", href: `${root}Miscellaneous/Misc.html` },
         
     ]
-const blogPages =
+const blogPa =
     [
-        {name :"Blog Posts"},
+        {name :"Post 1"},
+        {name :"Post 2"},
+        {name :"Post 3"},
+        {name :"Post 4"},
+        {name :"Post 5"},
+        {name :"Post 6"},
+        {name :"Post 7"},
+        {name :"Post 8"},
     ]
 export function initialise(currentPage) {
     const nav = document.querySelector("header > nav")
