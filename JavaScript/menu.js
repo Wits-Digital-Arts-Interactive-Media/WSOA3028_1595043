973e401dd56c256673ecbcd73e4631dedbe3d34d
Etsalot <ettienne.swart95@gmail.com>
2024-04-26 16:10:09 +0200

    update

--- JavaScript/menu.js
+++ JavaScript/menu.js
@@ -1,26 +1,16 @@
-const root = "/"
+const root = "/WSOA3028_1595043"
 const menuItems =
     [
-        { name: "Home", href: `${root}index.html`},
-        { name: "Blog", href: `${root}Blog Posts/index.html` },
-        { name: "Essays", href: `${root}Essay/Essay.html` },
-        { name: "Portfolio", href: `${root}Portfolio/Portfolio.html` },
-        { name: "Profile", href: `${root}Profile/about.html` },
-        { name: "Design", href: `${root}Design/Design.html` },
-        { name: "Misc", href: `${root}Miscellaneous/Misc.html` },
+        { name: "Home", href: root + "/index.html"},
+        { name: "Blog", href: `${root}/Blog Posts/index.html` },
+        { name: "Essays", href: `${root}/Essay/index.html` },
+        { name: "Portfolio", href: `${root}/Portfolio/Portfolio.html` },
+        { name: "Profile", href: `${root}/Profile/about.html` },
+        { name: "Design", href: `${root}/Design/Design.html` },
+        { name: "Misc", href: `${root}/Miscellaneous/Misc.html` },
         
     ]
-const blogPa =
-    [
-        {name :"Post 1"},
-        {name :"Post 2"},
-        {name :"Post 3"},
-        {name :"Post 4"},
-        {name :"Post 5"},
-        {name :"Post 6"},
-        {name :"Post 7"},
-        {name :"Post 8"},
-    ]
+
     
 export function initialise(currentPage) {
     const nav = document.querySelector("header > nav")
