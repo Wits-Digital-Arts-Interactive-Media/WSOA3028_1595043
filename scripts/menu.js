44191ec7130f7f36a593d96208d03a839e2817f6
Etsalot <ettienne.swart95@gmail.com>
2024-04-26 17:09:13 +0200

    update

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,6 +1,9 @@
 const root = "/WSOA3028_1595043"
+
+
+
 const menuItems =
-    [
+[
         { name: "Home", href: root + "/index.html"},
         { name: "Blog", href: root + "/Blog Posts/index.html" },
         { name: "Essays", href: `${root}/Essay/index.html` },
@@ -9,13 +12,12 @@ const menuItems =
         { name: "Design", href: `${root}/Design/Design.html` },
         { name: "Misc", href: `${root}/Miscellaneous/Misc.html` },
         
-    ]
+]
 
     
 export function initialise(currentPage) {
     const nav = document.querySelector("header > nav")
     const ul = document.createElement("ul")
-
     for (let menuItem of menuItems) {
         const li = document.createElement("li")
         if (currentPage != menuItem.name) {
@@ -23,7 +25,7 @@ export function initialise(currentPage) {
             a.innerText = menuItem.name
             a.setAttribute("href", menuItem.href)
             li.appendChild(a)
-        } else {li.innerText = menuItem.name}
+        } else { li.innerText = menuItem.name }
         ul.appendChild(li)
     }
     nav.appendChild(ul)
