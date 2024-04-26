a55b8caedadd3730fe9cc681afc5255f95c5d344
Etsalot <ettienne.swart95@gmail.com>
2024-04-26 15:04:32 +0200

    Update menu.js

--- JavaScript/menu.js
+++ JavaScript/menu.js
@@ -1,13 +1,13 @@
-const root = "/WSOA3028_1595043/"
+const root = "/WSOA3028_1595043"
 const menuItems =
     [
-        { name: "Home", href: `${root}index.html` },
-        { name: "Blog", href: `${root}Blog Posts/index.html` },
-        { name: "Essays", href: `${root}Essay/Essay.html` },
-        { name: "Portfolio", href: `${root}Portfolio/Portfolio.html` },
-        { name: "Profile", href: `${root}Profile/about.html` },
-        { name: "Design", href: `${root}Design/Design.html` },
-        { name: "Misc", href: `${root}Miscellaneous/Misc.html` },
+        { name: "Home", href: `${root}/index.html` },
+        { name: "Blog", href: `${root}/Blog Posts/index.html` },
+        { name: "Essays", href: `${root}/Essay/Essay.html` },
+        { name: "Portfolio", href: `${root}/Portfolio/Portfolio.html` },
+        { name: "Profile", href: `${root}/Profile/about.html` },
+        { name: "Design", href: `${root}/Design/Design.html` },
+        { name: "Misc", href: `${root}/Miscellaneous/Misc.html` },
         
     ]
 const blogPa =
