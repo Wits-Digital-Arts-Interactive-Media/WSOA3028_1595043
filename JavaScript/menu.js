8a6b349fa013348847158e036cdd4ac5e1db2613
Etsalot <ettienne.swart95@gmail.com>
2024-04-26 15:13:02 +0200

    update

--- JavaScript/menu.js
+++ JavaScript/menu.js
@@ -1,13 +1,13 @@
-const root = "WSOA3028_1595043"
+const root = "/WSOA3028_1595043/"
 const menuItems =
     [
-        { name: "Home", href: `${root}/index.html` },
-        { name: "Blog", href: `${root}/Blog Posts/index.html` },
-        { name: "Essays", href: `${root}/Essay/Essay.html` },
-        { name: "Portfolio", href: `${root}/Portfolio/Portfolio.html` },
-        { name: "Profile", href: `${root}/Profile/about.html` },
-        { name: "Design", href: `${root}/Design/Design.html` },
-        { name: "Misc", href: `${root}/Miscellaneous/Misc.html` },
+        { name: "Home", href: `${root}index.html` },
+        { name: "Blog", href: `${root}Blog Posts/index.html` },
+        { name: "Essays", href: `${root}Essay/Essay.html` },
+        { name: "Portfolio", href: `${root}Portfolio/Portfolio.html` },
+        { name: "Profile", href: `${root}Profile/about.html` },
+        { name: "Design", href: `${root}Design/Design.html` },
+        { name: "Misc", href: `${root}Miscellaneous/Misc.html` },
         
     ]
 const blogPa =
@@ -25,24 +25,20 @@ const blogPa =
 {
     return root;
 }
-function initialize() {
+export function initialise(currentPage) {
     const nav = document.querySelector("header > nav")
     const ul = document.createElement("ul")
 
-    for (const menuItem of menuItems) {
+    for (let menuItem of menuItems) {
         const li = document.createElement("li")
-        if (menuItem.name != document.title) {
+        if (currentPage != menuItem.name) {
             const a = document.createElement("a")
             a.innerText = menuItem.name
             a.setAttribute("href", menuItem.href)
             li.appendChild(a)
-        }
-        else if (menuItem.name == document.title) {
-            li.innerText = menuItem.name
-        }
+           
+        } else (li.innerText = menuItem.name)
         ul.appendChild(li)
     }
     nav.appendChild(ul)
-
 }
-initialize()
