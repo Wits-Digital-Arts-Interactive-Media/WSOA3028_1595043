fb63be1faf8f6eaaa0213bcb3cd22b0cb0fa9df9
Etsalot <ettienne.swart95@gmail.com>
2024-04-27 12:20:47 +0200

    major update

--- scripts/menu.js
+++ scripts/menu.js
@@ -15,16 +15,16 @@ const menuItems =
 ]
 
 const BlogPages = [
-    { name: "Page 1", href: root + "/Blog Posts/Post 1.html"},
-    { name: "Page 2", href: root + "/Blog Posts/Post 2.html"},
-    { name: "Page 3", href: root + "/Blog Posts/Post 3.html"},
-    { name: "Page 4", href: root + "/Blog Posts/Post 4.html"},
-    { name: "Page 5", href: root + "/Blog Posts/Post 5.html"},
-    { name: "Page 6", href: root + "/Blog Posts/Post 6.html"},
-    { name: "Page 7", href: root + "/Blog Posts/Post 7.html"},
-    { name: "Page 8", href: root + "/Blog Posts/Post 8.html"},
-    { name: "Page 9", href: root + "/Blog Posts/Post 9.html"},
-    { name: "Page 10", href: root + "/Blog Posts/Post 10.html"},
+    { name: "Blog Home", href: "/Blog Posts/index.html" },
+    { name: "Page 1", href:  "/Blog Posts/Post 1.html"},
+    { name: "Page 2", href:  "/Blog Posts/Post 2.html"},
+    { name: "Page 3", href:  "/Blog Posts/Post 3.html"},
+    { name: "Page 4", href:  "/Blog Posts/Post 4.html"},
+    { name: "Page 5", href:  "/Blog Posts/Post 5.html"},
+    { name: "Page 6", href:  "/Blog Posts/Post 6.html"},
+    { name: "Page 7", href:  "/Blog Posts/Post 7.html"},
+    { name: "Page 8", href:  "/Blog Posts/Post 8.html"},
+    
 ]
 
     
@@ -46,17 +46,17 @@ export function initialise(currentPage) {
 
 export function Pageinitialise(currentPage) {
     const nav = document.querySelector("header > nav")
-    const ul = document.createElement("ul")
+    const u2 = document.createElement("u2")
     for (let BlgPage of BlogPages) {
-        const li = document.createElement("li")
+        const lj = document.createElement("lj")
         if (currentPage != BlgPage.name) {
-            const a = document.createElement("a")
-            a.innerText = BlgPage.name
-            a.setAttribute("href", BlgPage.href)
-            li.appendChild(a)
-        } else { li.innerText = BlgPage.name }
-        ul.appendChild(li)
+            const b = document.createElement("b")
+            b.innerText = BlgPage.name
+            b.setAttribute("href", BlgPage.href)
+            lj.appendChild(b)
+        } else { lj.innerText = BlgPage.name }
+        u2.appendChild(lj)
     }
-    nav.appendChild(ul)
+    nav.appendChild(u2)
 }
 
